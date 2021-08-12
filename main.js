0b1da5cd6263e833de455d029efd9f337518244c
ren1244 <n1244506804@hotmail.com>
2021-08-12 16:10:08 +0800

    add cfb

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import * as Des from './lib/des.js';
 import * as TripleDes from './lib/triple-des.js';
 import { CBC } from './lib/cbc.js';
 import { ECB } from './lib/ecb.js';
+import { CFB } from './lib/cfb.js';
 
 const paddingMap = {
     "PKCS#7": Padding.Pkcs7,
@@ -22,8 +23,22 @@ const encodingMap = {
 };
 
 const modeMap = {
-    'cbc': CBC,
-    'ecb': ECB
+    'cbc': {
+        obj: CBC,
+        blockSize: 8,
+    },
+    'ecb': {
+        obj: ECB,
+        blockSize: 8,
+    },
+    'cfb-8': {
+        obj: CFB,
+        blockSize: 1,
+    },
+    'cfb-64': {
+        obj: CFB,
+        blockSize: 8,
+    }
 };
 
 const cipherAlgCheck = {
@@ -69,16 +84,16 @@ document.querySelector('#encryptoBtn').addEventListener('click', () => {
     let cipherAlg = document.querySelector('input[name="cipherAlg"]:checked').value;
     let paddingMethod = document.querySelector('select[name="padding"]').value;
     let mode = document.querySelector('input[name="mode"]:checked').value;
-    let alg = new modeMap[mode]({
+    let alg = new modeMap[mode].obj({
         cipherAlg: cipherMap[cipherAlg],
         paddingAlg: paddingMap[paddingMethod],
-        blockSize: 8
+        blockSize: modeMap[mode].blockSize
     });
     let ivEle = document.querySelector('#iv');
     let iv = ivEle.value.replace(/\s+/g, '');
     let key = document.querySelector('#key').value.replace(/\s+/g, '');
     //檢查 iv 正確性
-    if(!ivEle.disabled) {
+    if (!ivEle.disabled) {
         if (iv.search(/^[0-9a-fA-F]{16}$/) < 0) {
             alert('初始向量必須為 16 個十六進位字元');
             return;
@@ -92,13 +107,17 @@ document.querySelector('#encryptoBtn').addEventListener('click', () => {
     }
     key = Coder.Hex.enc(key);
     let encoding = document.querySelector('input[name="encoding"]:checked').value;
-    let msg = document.querySelector('#plain').value;
-    if (encoding === 'hex' && (msg.search(/^[0-9a-fA-F]+$/) < 0 || msg.length % 2 === 1)) {
+    let origMsg = document.querySelector('#plain').value;
+    if (encoding === 'hex' && (origMsg.search(/^[0-9a-fA-F]+$/) < 0 || origMsg.length % 2 === 1)) {
         alert('原始資料格式錯誤');
     }
-    msg = encodingMap[encoding](msg);
+    let msg = encodingMap[encoding](origMsg);
     let result = ivEle.disabled ? alg.encrypt(msg, key) : alg.encrypt(msg, key, iv);
     let ele = document.querySelector('#output');
     ele.parentElement.removeAttribute('style');
     ele.value = Coder.Hex.dec(result);
+    //解密回去驗證是否等於原始訊息
+    let decResult = ivEle.disabled ? alg.decrypt(result, key,) : alg.decrypt(result, key, iv);
+    let decMsg = Coder[{ utf8: 'Utf8', hex: 'Hex' }[encoding]].dec(decResult);
+    document.querySelector('#decResult').textContent = '解密測試：' + (decMsg === origMsg ? '正確' : '錯誤');
 });
